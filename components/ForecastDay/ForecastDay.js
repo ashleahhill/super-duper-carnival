be339b1c204c72241d999782f05f8fa5c5bcf2b4
Ashley Hill <me@ashleyhill.me>
2016-10-15 11:11:29 -0400

    Add SASS compiling #12

--- components/ForecastDay/ForecastDay.js
+++ components/ForecastDay/ForecastDay.js
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react';
 import { map } from 'lodash';
 
+import s from './ForecastDay.css';
+import z from './ForecastDay.scss';
 import Weather from './../Weather';
 
 class ForecastDay extends React.Component {
@@ -11,13 +13,13 @@ class ForecastDay extends React.Component {
 
 
   render() {
-
     let length = this.props.length || 10;
 
     let forecasts = this.props.forecasts.slice(0,length);
 
     return (
-      <div className={this.props.className}>
+      <div className={this.props.className + ' ' + s['forecast-day']}>
+        <span className={z['test-2']}>Test</span>
         {
           map(forecasts, (forecast, i) => {
             return (<Weather key={i} weatherData={forecast} hourly={true}></Weather>)
