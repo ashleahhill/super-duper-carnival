e6a45b61a013f504d8fbe72b9957d17623a6ef1c
Ashley L Hill <ashleahhill@gmail.com>
2016-10-19 13:43:37 -0400

    Add gulp (#41)
    closes #11 
    
    * Separate run scripts from cli #11
    
    * Add Gulp task for deployment #11
    
    * Bumps to version 0.0.1
    
    * Fix error handling on createRelease #11
    
    * Bumps to version 0.0.2
    
    * Bumps to version 0.0.3
    
    * Check for correct branch before deploying #11
    
    * Add deploy instructions #11

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ Clone the repository and run `npm install` in the root directory.
 
 ```bash
 $ npm install
-
 ```
 
 You will need a configuration file to build the app. An example configuration file can be found in `example.config.json`.
@@ -32,10 +31,40 @@ Run npm start to run the app locally
 $ npm start
 ```
 
-
 Built with [generator-react-static](https://github.com/kriasoft/react-static-boilerplate).
 
 
-### License
+## Deploying
+
+Automatic deployment to firebase can be triggered with a [gulp](http://gulpjs.com/) task.
+
+Install gulp-cli
+
+```bash
+$ npm install --global gulp-cli
+```
+
+You must be on the master branch to deploy
+
+```bash
+$ git checkout master
+```
+
+Run the deploy task with an option specifying the version to bump
+
+```bash
+$ gulp deploy # default, patch version
+
+$ gulp deploy --major # major version
+
+$ gulp deploy --minor # minor version
+
+$ gulp deploy --patch # patch version
+```
+
+Uses [gulp-github-release-maker](https://github.com/IcanDivideBy0/gulp-github-release-maker)
+
+
+## License
 Copyright © 2015-present Ashley Hill. This source code is licensed under the MIT license found in [LICENSE.txt](https://github.com/kriasoft/react-static-boilerplate/blob/master/LICENSE.txt) file.
 
---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const gulp = require('gulp');
+const githubRelease = require('gulp-github-release-maker');
+const runTask = require('./run-tasks');
+const git = require('gulp-git');
+
+
+gulp.task('release:changelog', function (done) {
+  githubRelease.showChangelog(done);
+});
+
+gulp.task('release:do:patch', function (done) {
+  githubRelease.createRelease({ type: 'patch' }, done);
+});
+
+gulp.task('release:do:minor', function (done) {
+  githubRelease.createRelease({ type: 'minor' }, done);
+});
+
+gulp.task('release:do:major', function (done) {
+  githubRelease.createRelease({ type: 'major' }, done);
+});
+
+/**
+ * Wrap gulp-github-release-maker in a promise
+ *
+ * @param {any} type
+ * @returns
+ */
+function releaseReturnPromise (type) {
+  return new Promise((resolve, reject) => {
+    githubRelease.createRelease({ type: type }, (err, result)=> {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
+    });
+
+  })
+}
+
+/**
+ * pull semver to increment from cli option
+ *
+ * @param {any} argv
+ * @returns
+ */
+function getVersionType (argv) {
+  let versionType = 'patch';
+
+  versionType = (argv.indexOf('--major') > -1) ? 'major' : versionType;
+  versionType = (argv.indexOf('--minor') > -1) ? 'minor' : versionType;
+
+  return versionType;
+}
+
+/**
+ * Check if on master
+ */
+gulp.task('check-master', function (done) {
+  git.exec({
+    args: 'rev-parse --abbrev-ref HEAD',
+    quiet: true
+  }, (err, stdout) => {
+
+    if (err) {
+      throw err;
+    }
+    if (stdout === 'master') {
+      done();
+      return;
+    }
+    done(new Error(`You must be on \`master\` branch to tag and deploy. Currently on branch: ${stdout}.`));
+  });
+})
+
+/**
+ * Build, tag, and publish
+ */
+gulp.task('deploy', ['check-master'], function (done) {
+
+  const versionType = getVersionType(process.argv);
+
+  runTask('build')
+    .then(() => {
+      return releaseReturnPromise(versionType);
+    })
+    .then(() => {
+      return runTask('publish-post-tag')
+    })
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
+
