7a3b7a88d0112181f01973876f0dd6259e37d40b
Ashley L Hill <ashleahhill@gmail.com>
2016-10-19 02:51:54 -0400

    Setup hosting (#22)
    Closes #17, closes #13
    
    * Fix Layout header property error
    
    * Add crossorigin.me to production build #17 #13

--- core/services/darkSky.js
+++ core/services/darkSky.js
@@ -4,6 +4,8 @@ const norfolk = {
   lng: -76.3256277
 };
 
+const apiPath = __DEV__ ? '/api/weather/' : `https://crossorigin.me/https://api.darksky.net/forecast/${darkSky}/`; // eslint-disable-line no-undef
+
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response
@@ -31,10 +33,9 @@ function makeCall(url) {
 }
 
 export function getCurrent(latLng = norfolk) {
-
-  return makeCall(`/api/weather/${latLng.lat},${latLng.lng}?exclude=minutely,flags`);
+  return makeCall(`${apiPath}${latLng.lat},${latLng.lng}?exclude=minutely,flags`);
 }
 
 export function getFuture(time, latLng = norfolk) {
-  return makeCall(`/api/weather/${latLng.lat},${latLng.lng},${time}?exclude=minutely,flags`);
+  return makeCall(`${apiPath}${latLng.lat},${latLng.lng},${time}?exclude=minutely,flags`);
 }
